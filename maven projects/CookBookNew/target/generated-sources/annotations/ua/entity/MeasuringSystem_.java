60b6dd870c6eb881b7bbce1e70687b0a511ebb91
Sviatoslav Naida <sviatoslav.naida@gmail.com>
2017-12-14 20:44:31 -0500

    2 working maven projects
    coock book its little advanced
    but other one its almost like we do on session

---
+++ maven projects/CookBookNew/target/generated-sources/annotations/ua/entity/MeasuringSystem_.java
@@ -0,0 +1,16 @@
+package ua.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(MeasuringSystem.class)
+public abstract class MeasuringSystem_ extends ua.entity.AbstractEntity_ {
+
+	public static volatile ListAttribute<MeasuringSystem, Amount> amounts;
+	public static volatile SingularAttribute<MeasuringSystem, String> name;
+
+}
+
